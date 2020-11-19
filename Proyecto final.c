569a42404a8fa259a45065a347c35d6047d637cd
DaveAcevegam <73979188+DaveAcevegam@users.noreply.github.com>
2020-11-19 11:13:24 -0600

    Update Proyecto final.c

--- Proyecto final.c
+++ Proyecto final.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 void iniciarTablero();
 void mostrarTablero(int band); 
 void leerCoordenadas(FILE *f);
+void archivo(char nombre[6], int edad, int ganada, int barcos);
 
 int tablero[10][10];
 
@@ -92,4 +94,29 @@ void leerCoordenadas(FILE *f)
 	}
 } 
 
-.
+void archivo(char nombre[6], int edad, int ganada, int barcos){
+   /*la funcion recibe 4 parametros
+   char nombre[10]: es la cadena del nombre
+   int edad: la edad
+   int ganada: recibe un 1 o un 0, si el jugador gana guardara 1
+               y si pierde guardara un 0
+   int barcos: recibe el numero de barcos que logo hundir el perdedor*/
+
+   FILE * flujo;
+   flujo = fopen("informacion.txt", "a");
+   if(flujo == NULL){
+      perror("Error al crear el archivo\n");
+   } else {
+      if(ganada==1){
+        fprintf(flujo, "%s     %d     ganador",nombre, edad);
+        fputc('\n', flujo);
+      } else{
+        fprintf(flujo, "%s     %d     perdedor  %d    barcos hundidos",nombre, edad, barcos);
+        fputc('\n', flujo);
+      }
+      fflush(flujo);
+      fclose(flujo);
+      printf("DATOS GUARDADOS EXITOSAMENTE!!!!!\n\n");
+   }
+   
+}	
